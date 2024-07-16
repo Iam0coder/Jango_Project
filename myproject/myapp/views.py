641c23278d89f677883fd42c777b28a54fa7e7ff
1nffo <90951973+1nffo@users.noreply.github.com>
2024-07-17 02:39:16 +0300

    upd prj

---
+++ myproject/myapp/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def data_view(request):
+    return HttpResponse("<h1>Data Page</h1><p>This is the data page.</p>")
+
+
+def test_view(request):
+    return HttpResponse("<h1>Test Page</h1><p>This is the test page.</p>")
